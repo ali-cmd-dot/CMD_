4bf7909b4254b4e89153be197073f8cf4d6e0021
ali-cmd-dot <ali@cautio.in>
2025-11-19 13:48:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   env: {
     GOOGLE_SHEETS_API_KEY: process.env.GOOGLE_SHEETS_API_KEY,
   },
